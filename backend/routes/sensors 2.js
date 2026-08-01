47bcce22866226028dbc3e1700eee2d0bcbccb85
ayaanrustagi <ayaan.rustagi2010@gmail.com>
2026-08-01 16:09:24 -0500

    Fix squished UI on mobile by overriding 70% max-width

--- backend/routes/sensors 2.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sensorController = require('../controllers/sensorController');
-
-router.get('/box-status', sensorController.getBoxStatus);
-router.post('/box-status', sensorController.postBoxStatus);
-
-module.exports = router;
